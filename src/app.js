8c36a06290a1cc31219de68d3f4f2f7bf7ef6172
TandiweBeta <tandiwebeta@gmail.com>
2024-05-07 10:17:21 +0200

    added axios and api

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
-function handleSearchSubmit (event){
+
+
+function updateWeather(response){
+let temperatureElement = document.querySelector("#temperature");
+let temperature = response.data.temperature.current;
+let cityElement = document.querySelector("#city")
+cityElement.innerHTML = response.data.city;
+temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function changeCity(city){
+let apiKey = "00c99d472fbddtac3e3b2d017a4afdbo";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}units=metric`;
+console.log(apiUrl)
+ axios.get(apiUrl).then(updateWeather); 
+}
+
+function handleSearchSubmit(event){
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
-
+  changeCity(searchInput.value);
 
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",handleSearchSubmit);
+
+changeCity("Johannesburg");
