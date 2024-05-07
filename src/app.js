efacaee2e599dc0914a55cbae83ec8fa8c95952f
TandiweBeta <tandiwebeta@gmail.com>
2024-05-07 19:05:24 +0200

    added more weather data

--- src/app.js
+++ src/app.js
@@ -3,14 +3,45 @@
 function updateWeather(response){
 let temperatureElement = document.querySelector("#temperature");
 let temperature = response.data.temperature.current;
-let cityElement = document.querySelector("#city")
+let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#description");
+let humidityElement =  document.querySelector("#Humidity");
+let windSpeedElement = document.querySelector("#wind-speed");
+let timeElement = document.querySelector("#Time");
+let date = new Date(response.data.time * 1000);
+
+
 cityElement.innerHTML = response.data.city;
+timeElement.innerHTML = formatDate(date);
+humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+descriptionElement.innerHTML = response.data.condition.description;
+windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 temperatureElement.innerHTML = Math.round(temperature);
+}
+function formatDate(date){
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let days = [
+      "Sunday", 
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+       "Thursday", 
+       "Friday", 
+       "Saturday"
+      ] ;
+
+let day = days[date.getDay()];
+if (minutes < 10){
+  minutes = `0${minutes}`;
+}
+
+return `${day} ${hours}:${minutes}`;
 }
 
 function changeCity(city){
 let apiKey = "00c99d472fbddtac3e3b2d017a4afdbo";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}units=metric`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 console.log(apiUrl)
  axios.get(apiUrl).then(updateWeather); 
 }
