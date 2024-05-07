37056e20d280d904dc098cbc76e0e21be51fd724
TandiweBeta <tandiwebeta@gmail.com>
2024-05-07 20:17:24 +0200

    added weather icon and adjusted the css accordinglyhe weather icon

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ let humidityElement =  document.querySelector("#Humidity");
 let windSpeedElement = document.querySelector("#wind-speed");
 let timeElement = document.querySelector("#Time");
 let date = new Date(response.data.time * 1000);
+let iconElement = document.querySelector("#icon");
 
 
 cityElement.innerHTML = response.data.city;
@@ -17,6 +18,7 @@ humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 descriptionElement.innerHTML = response.data.condition.description;
 windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 temperatureElement.innerHTML = Math.round(temperature);
+iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`; 
 }
 function formatDate(date){
 let hours = date.getHours();
